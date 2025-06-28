d04e04b96563b474f459a84878c91c53aeb62ad1
Nathan Kleekamp <nkleekamp@gmail.com>
2025-06-27 22:28:28 -0400

    test: add test for 'divide' function

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply } = require('./calculator');
+const { add, subtract, multiply, divide } = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
@@ -10,4 +10,8 @@ test('subtracts 5 - 2 to equal 3', () => {
 
 test('multiplies 3 * 4 to equal 12', () => {
   expect(multiply(3, 4)).toBe(12);
+});
+
+test('divides 10 / 2 to equal 5', () => {
+  expect(divide(10, 2)).toBe(5);
 });
